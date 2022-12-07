Shapovaloff/taskmanager-17

3.2.3 Добавит новый компонент - заглушку
- Создаем новый компонент - заглушку
- По условию - нет задач - рендерим заглушку на страницу
- Если задачи есть, то показываем компонент сортировки и карточки

N.B. Чтобы проверить, что заглушка показывается, сгенерируем пустой массив задач

src/presenter/board-presenter.js
@@ -4,6 +4,7 @@ import TaskListView from '../view/task-list-view.js';
 import TaskView from '../view/task-view.js';
 import TaskEditView from '../view/task-edit-view.js';
 import LoadMoreButtonView from '../view/load-more-button-view.js';
+import NoTaskView from '../view/no-task-view.js';
 import {render} from '../render.js';
 
 const TASK_COUNT_PER_STEP = 8;
@@ -25,17 +26,22 @@ export default class BoardPresenter {
     this.#boardTasks = [...this.#tasksModel.tasks];
 
     render(this.#boardComponent, this.#boardContainer);
-    render(new SortView(), this.#boardComponent.element);
-    render(this.#taskListComponent, this.#boardComponent.element);
 
-    for (let i = 0; i < Math.min(this.#boardTasks.length, TASK_COUNT_PER_STEP); i++) {
-      this.#renderTask(this.#boardTasks[i]);
-    }
+    if (this.#boardTasks.every((task) => task.isArchive)) {
+      render(new NoTaskView(), this.#boardComponent.element);
+    } else {
+      render(new SortView(), this.#boardComponent.element);
+      render(this.#taskListComponent, this.#boardComponent.element);
 
-    if (this.#boardTasks.length > TASK_COUNT_PER_STEP) {
-      render(this.#loadMoreButtonComponent, this.#boardComponent.element);
+      for (let i = 0; i < Math.min(this.#boardTasks.length, TASK_COUNT_PER_STEP); i++) {
+        this.#renderTask(this.#boardTasks[i]);
+      }
 
-      this.#loadMoreButtonComponent.element.addEventListener('click', this.#handleLoadMoreButtonClick);
+      if (this.#boardTasks.length > TASK_COUNT_PER_STEP) {
+        render(this.#loadMoreButtonComponent, this.#boardComponent.element);
+
+        this.#loadMoreButtonComponent.element.addEventListener('click', this.#handleLoadMoreButtonClick);
+      }
     }
   };
 

Alex-345876/htmlacademy

src/presenter/board-presenter.js
@@ -4,6 +4,7 @@ import TaskListView from '../view/task-list-view.js';
 import TaskView from '../view/task-view.js';
 import TaskEditView from '../view/task-edit-view.js';
 import LoadMoreButtonView from '../view/load-more-button-view.js';
+import NoTaskView from '../view/no-task-view.js';
 import {render} from '../render.js';
 
 const TASK_COUNT_PER_STEP = 8;
@@ -28,18 +29,23 @@ export default class BoardPresenter {
     this.#boardTasks = [...this.#tasksModel.tasks];
 
     render(this.#boardComponent, this.#boardContainer);
-    render(new SortView(), this.#boardComponent.element);
-    render(this.#taskListComponent, this.#boardComponent.element);
 
-    for (let i = 0; i < Math.min(this.#boardTasks.length, TASK_COUNT_PER_STEP); i++) {
-      this.#renderTask(this.#boardTasks[i]);
-    }
+    if (this.#boardTasks.every((task) => task.isArchive)) {
+      render(new NoTaskView(), this.#boardComponent.element);
+    } else {
+      render(new SortView(), this.#boardComponent.element);
+      render(this.#taskListComponent, this.#boardComponent.element);
 
-    if (this.#boardTasks.length > TASK_COUNT_PER_STEP) {
-      this.#loadMoreButtonComponent = new LoadMoreButtonView();
-      render(this.#loadMoreButtonComponent, this.#boardComponent.element);
+      for (let i = 0; i < Math.min(this.#boardTasks.length, TASK_COUNT_PER_STEP); i++) {
+        this.#renderTask(this.#boardTasks[i]);
+      }
 
-      this.#loadMoreButtonComponent.element.addEventListener('click', this.#loadMoreButtonClickHandler);
+      if (this.#boardTasks.length > TASK_COUNT_PER_STEP) {
+        this.#loadMoreButtonComponent = new LoadMoreButtonView();
+        render(this.#loadMoreButtonComponent, this.#boardComponent.element);
+
+        this.#loadMoreButtonComponent.element.addEventListener('click', this.#loadMoreButtonClickHandler);
+      }
     }
   }
 