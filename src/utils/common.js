wonderjs/taskmanager-18

7.3 Доработает модель и датабиндинг. WIP
- Опишем в модели методы добавления, обновления и удаления задачи
- #handleTaskChange заменим на обработчик любого пользовательского действия (пока пустой)
- добавим обработчик-наблюдатель #handleModelEvent, который будет реагировать на изменения модели

WIP: линтер ругается на консоль. По-прежнему не работает обновление

src/utils/common.js
@@ -7,18 +7,4 @@ const getRandomInteger = (a = 0, b = 1) => {
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
-const updateItem = (items, update) => {
-  const index = items.findIndex((item) => item.id === update.id);
-
-  if (index === -1) {
-    return items;
-  }
-
-  return [
-    ...items.slice(0, index),
-    update,
-    ...items.slice(index + 1),
-  ];
-};
-
-export {getRandomInteger, updateItem};
+export {getRandomInteger};

Alex-345876/htmlacademy

src/utils/common.js
@@ -2,8 +2,4 @@ function getRandomArrayElement(items) {
   return items[Math.floor(Math.random() * items.length)];
 }
 
-function updateItem(items, update) {
-  return items.map((item) => item.id === update.id ? update : item);
-}
-
-export {getRandomArrayElement, updateItem};
+export {getRandomArrayElement};